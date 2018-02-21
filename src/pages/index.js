65a422d11daf69c6976d52521346d5aee4b47edc
Mateus Dal Bianco <mateus@dalbinaco.com>
2018-02-21 19:32:12 -0300

    Remove logo listing src

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,6 @@ import Features from '../components/Features'
 import About from '../components/About'
 import Logos from '../components/Logos'
 
-const logo = require('../assets/img/logos/1.svg')
-
 const IndexPage = () => (
   <div>
     <Main
