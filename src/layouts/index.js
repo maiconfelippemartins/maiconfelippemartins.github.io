99ea1293d26304f6b1e06f77475521db5f8fc622
Mateus Dal Bianco <mateus@dalbinaco.com>
2018-02-22 10:50:29 -0300

    source favicons

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -15,7 +15,15 @@ const TemplateWrapper = ({ children }) => (
       meta={[
         { name: 'description', content: 'Gerenciamento das redes sociais de sua empresa, identidade visual da sua marca, criação de material impresso e digital e ilustrações ' },
       ]}
-    />
+    >
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="theme-color" content="#fff" />
+    </Helmet>
     <div style={{flex: 1}}>
       <div>
         {children()}
